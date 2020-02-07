3780d2559babbaa57fd6727f9da6126d78f25ca3
Cristian Alexander Martinez <40245786+AlxZeroX@users.noreply.github.com>
2020-02-07 11:17:39 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int main(){
+char cadena[100];
+int indice,l,contador=0,leyoletras=0;
+printf("\ningrese una frase  :");
+gets(cadena);
+l= strlen(cadena);
+
+for(indice=0;indice<l;indice++)
+{
+ if(cadena[indice]==' ')
+ {
+  if(leyoletras==1)
+   if(cadena [indice+1]!=' ')
+    contador=contador+1;
+         }
+   else
+    leyoletras=1;
+   }
+  
+if(cadena[indice-1]!=' '){
+ contador=contador+1;
+}
+printf("\nel numero de palabras de la frase es: %d",contador);
+ 
+ 
+ return 0;
+}
